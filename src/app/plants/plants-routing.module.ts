cb4d6c7a5494289e667f7d5ee99aa9a04364709d
Chloe Guglielmi <chloe.guglielmi@teachonmars.com>
2020-04-13 11:34:07 +0200

    Moving components

--- src/app/plants/plants-routing.module.ts
+++ src/app/plants/plants-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {IndoorListComponent} from './pages/indoor-list/indoor-list.component';
-import {OutdoorListComponent} from './pages/outdoor-list/outdoor-list.component';
+import {IndoorListComponent} from './pages/plants/indoor-list/indoor-list.component';
+import {OutdoorListComponent} from './pages/plants/outdoor-list/outdoor-list.component';
 
 const appRoutes: Routes = [
   { path: 'indoor',
--- src/app/plants/plants.module.ts
+++ src/app/plants/plants.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PlantsRoutingModule} from './plants-routing.module';
-import { IndoorListComponent } from './pages/indoor-list/indoor-list.component';
-import { OutdoorListComponent } from './pages/outdoor-list/outdoor-list.component';
+import { IndoorListComponent } from './pages/plants/indoor-list/indoor-list.component';
+import { OutdoorListComponent } from './pages/plants/outdoor-list/outdoor-list.component';
 
 @NgModule({
   declarations: [IndoorListComponent, OutdoorListComponent],
